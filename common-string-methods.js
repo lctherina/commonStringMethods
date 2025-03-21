deface3bbfacd9257d66923003104d15cb35ec6c
Laura Therina <lctherina@gmail.com>
2025-03-20 19:09:45 -0500

    added codes to prompts

--- common-string-methods.js
+++ common-string-methods.js
@@ -69,23 +69,35 @@ Complete the following tasks and assign the results to the specified variables. 
 let inputString = "  Welcome to the Coding Bootcamp! Learn JavaScript today.  ";
 
 // 1. Searching
-let hasJavaScript; // Your code here
-let codingPosition; // Your code here
-let startsWithWelcome; // Your code here
-let endsWithToday; // Your code here
+let hasJavaScript = inputString.includes("JavaScript");
+console.log(hasJavaScript);
+let codingPosition = inputString.indexOf("Coding");
+console.log(codingPosition);
+let startsWithWelcome = inputString.startsWith("Welcome");
+console.log(startsWithWelcome); 
+let endsWithToday = inputString.endsWith("today.");
+console.log(endsWithToday);
 
 // 2. Transforming
-let lowercaseString; // Your code here
-let uppercaseString; // Your code here
-let trimmedString; // Your code here
-let replacedString; // Your code here
+let lowercaseString = inputString.toLowerCase();
+console.log(lowercaseString);
+let uppercaseString = inputString.toUpperCase();
+console.log(uppercaseString);
+let trimmedString = inputString.trim();
+console.log(trimmedString);
+let replacedString = inputString.replace("JavaScript", "coding");
+console.log(replacedString);
+
 
 // 3. Breaking Apart
-let wordsArray; // Your code here
+let wordsArray = inputString.split(" ");
+console.log(wordsArray);
 
 // 4. Retrieving
-let firstCharacter; // Your code here
-let extractedBootcamp; // Your code here
+let firstCharacter = trimmedString.charAt(0);
+console.log(firstCharacter);
+let extractedBootcamp = trimmedString.slice(21,30);
+console.log(extractedBootcamp);
 
 // Log all results
 console.log({
